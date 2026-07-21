9fa2e2e213f0641d49ed21304d92962b86960ff9
Heena27-1 <heenakousarshaik88@gmail.com>
2026-07-21 22:37:27 +0530

    Implement CustomUserDetailsService for JWT authentication

--- src/main/java/com/hirehub/hirehub_backend/security/CustomUserDetailsService.java
+++ src/main/java/com/hirehub/hirehub_backend/security/CustomUserDetailsService.java
@@ -1,5 +1,31 @@
 package com.hirehub.hirehub_backend.security;
 
-public class CustomUserDetailsService {
-    
+import com.hirehub.hirehub_backend.entity.User;
+import com.hirehub.hirehub_backend.repository.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
 }
